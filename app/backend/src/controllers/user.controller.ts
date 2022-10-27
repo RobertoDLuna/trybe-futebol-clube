41ecfda26a1191fe3ef593478d28827ab8fe2492
Roberto Luna <robertocgw@gmail.com>
2022-10-27 17:01:43 -0300

    adição do user controller

---
+++ app/backend/src/controllers/user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import UserService from '../services/user.service';
+
+export default class UserController {
+  constructor(private userService: UserService) {}
+
+  async login(req: Request, res: Response) {
+    try {
+      const login = req.body;
+      const token = await this.userService.login(login);
+
+      if (!token) return res.status(401).json({ message: 'Incorrect email or password' });
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(401).json({ message: 'All fields must be filled' });
+    }
+  }
+
+  async validateLogin(req: Request, res: Response) {
+    try {
+      const { authorization } = req.headers;
+      const result = await this.userService.validateLogin(authorization);
+      res.status(200).json({ result: result.role });
+    } catch (error) {
+      res.status(401).json({ message: 'invalid token' });
+    }
+  }
+}
