cdcd804494fa5103325200ed60faa61d20171ec9
Roberto Luna <robertocgw@gmail.com>
2022-10-27 16:17:48 -0300

    adição do tokenAuth

---
+++ app/backend/src/auths/tokenAuth.ts
@@ -0,0 +1,23 @@
+import { sign, Secret, verify, JwtPayload } from 'jsonwebtoken';
+import { compareSync } from 'bcryptjs';
+import UserModel from '../database/models/user.model';
+
+export default class TokenAuth {
+  static compare(token: string, password: string): boolean {
+    return compareSync(token, password);
+  }
+
+  static encrypt(user: UserModel): string {
+    const secret = process.env.JWT_SECRET as Secret;
+    const token = sign(user, secret);
+
+    return token;
+  }
+
+  static decrypt(token: string) {
+    const secret = process.env.JWT_SECRET as Secret;
+    const decrypted = verify(token, secret);
+
+    return decrypted as JwtPayload;
+  }
+}
