6b94995f457cdeae8cdcba454eb7dd15a0dce386
Roberto Luna <robertocgw@gmail.com>
2022-10-27 16:43:22 -0300

    user service concluido

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -9,5 +9,23 @@ export default class UserService {
     if (!login.email || !login.password) throw new Error();
 
     const user = await this.userModel.findOne({ where: { email: login.email } });
+
+    if (!user) return null;
+
+    const validateUserPass = tokenAuth.compare(user.email, user.password);
+
+    if (validateUserPass) {
+      const token = tokenAuth.encrypt(user);
+      return token;
+    }
+    return null;
+  }
+
+  async validateLogin(auth: string | undefined) {
+    if (!auth) throw new Error();
+
+    const decrypted = tokenAuth.decrypt(auth);
+    const user = await this.userModel.findOne({ where: { password: decrypted } });
+    return user as UserModel;
   }
 }
