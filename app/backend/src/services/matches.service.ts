0333ed6a6364e0fe5f288f2388e8878426916286
Roberto Luna <robertocgw@gmail.com>
2022-11-01 15:22:36 -0300

    adição da service de matches e correção da tipagem da service de teams

---
+++ app/backend/src/services/matches.service.ts
@@ -0,0 +1,16 @@
+import TeamsModel from '../models/teams.model';
+import MatchesModel from '../models/matches.model';
+
+export default class MatchesService {
+  constructor(private matchesModel: typeof MatchesModel) {}
+
+  async getAllMatches(): Promise <MatchesModel []> {
+    const allMatches = await this.matchesModel.findAll({
+      include: [
+        { model: TeamsModel, as: 'teamHome', attributes: ['teamName'] },
+        { model: TeamsModel, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+    return allMatches;
+  }
+}
--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -3,12 +3,12 @@ import TeamsModel from '../models/teams.model';
 export default class TeamsService {
   constructor(private teamsModel: typeof TeamsModel) {}
 
-  async getAllTeams() {
+  async getAllTeams(): Promise <TeamsModel[]> {
     const allTeams = await this.teamsModel.findAll();
     return allTeams;
   }
 
-  async getById(id: number) {
+  async getById(id: number): Promise <TeamsModel | null> {
     const team = await this.teamsModel.findOne({ where: { id } });
     return team;
   }
