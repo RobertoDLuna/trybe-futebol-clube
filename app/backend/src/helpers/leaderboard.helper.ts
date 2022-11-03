bc559da1a5be1cba8901bc69ad7e4121e5a0ed4b
Roberto Luna <robertocgw@gmail.com>
2022-11-03 16:50:33 -0300

    adição das funções sortLeaderboard, calculateTotalGames e Calculategoalsfavor no helper de leaderboards

---
+++ app/backend/src/helpers/leaderboard.helper.ts
@@ -0,0 +1,31 @@
+import TeamsModel from '../models/teams.model';
+import IMatch from '../interfaces/IMatch';
+import ILeaderboard from '../interfaces/ILeaderboards';
+
+function sortLeaderboard(lboard: ILeaderboard[]) {
+  const sorted = lboard.sort((a: ILeaderboard, b: ILeaderboard) => {
+    if (a.totalPoints !== b.totalPoints) return b.totalPoints - a.totalPoints;
+    if (a.totalVictories !== b.totalVictories) return b.totalVictories - a.totalVictories;
+    if (a.goalsBalance !== b.goalsBalance) return b.goalsBalance - a.goalsBalance;
+    if (a.goalsFavor !== b.goalsFavor) return b.goalsFavor - a.goalsFavor;
+    return b.goalsOwn - a.goalsOwn;
+  });
+  return sorted;
+}
+
+export function calculateTotalGames(matches: IMatch[], teamId: number) {
+  const totalGames = matches.reduce((acc, curr) => {
+    if (curr.homeTeam === teamId || curr.awayTeam === teamId) return acc + 1;
+    return acc;
+  }, 0);
+  return totalGames;
+}
+
+export function calculateGoalsFavor(matches: IMatch[], teamId: number) {
+  const totalGoals = matches.reduce((acc, curr) => {
+    if (curr.homeTeam === teamId) return acc + curr.homeTeamGoals;
+    if (curr.awayTeam === teamId) return acc + curr.awayTeamGoals;
+    return acc;
+  }, 0);
+  return totalGoals;
+}
