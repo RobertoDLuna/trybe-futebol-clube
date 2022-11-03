83ee8bb7445388b5b41608797415d9c3676a00a8
Roberto Luna <robertocgw@gmail.com>
2022-11-03 12:53:42 -0300

    mudança no router de matches para uso dos outros models

--- app/backend/src/routes/matches.router.ts
+++ app/backend/src/routes/matches.router.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 import MatchesService from '../services/matches.service';
 import MatchesController from '../controllers/matches.controller';
 import MatchesModel from '../models/matches.model';
+import UserModel from '../models/user.model';
+import TeamsModel from '../models/teams.model';
 
 const MatchesRoutes = Router();
 
-const matchesController = new MatchesController(new MatchesService(MatchesModel));
+const matchesController = new MatchesController(new MatchesService(
+  MatchesModel,
+  UserModel,
+  TeamsModel,
+));
 
 MatchesRoutes.get('/Matches', (req, res) => matchesController.getAllMatches(req, res));
 
