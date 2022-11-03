39664d451c99885b3dbea8b637d73b4b103fcf57
Roberto Luna <robertocgw@gmail.com>
2022-11-03 13:21:08 -0300

    função para criação de nova partida

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,10 +1,14 @@
 import TeamsModel from '../models/teams.model';
 import MatchesModel from '../models/matches.model';
+import IMatch from '../interfaces/IMatch';
+import UserModel from '../models/user.model';
+import TokenAuth from '../auths/tokenAuth';
 
 export default class MatchesService {
   constructor(
     private matchesModel: typeof MatchesModel,
     private teamsModel: typeof TeamsModel,
+    private userModel: typeof UserModel,
   ) {}
 
   async getAllMatches(): Promise <MatchesModel []> {
@@ -31,5 +35,14 @@ export default class MatchesService {
   async findTeamById(id: number) {
     const team = await this.teamsModel.findOne({ where: { id } });
     return team;
+  }
+
+  async createNewMatch(match: IMatch, authorization: string | undefined) {
+    if (!authorization) throw new Error();
+    const { data } = TokenAuth.decrypt(authorization);
+    const user = await this.userModel.findOne({ where: { password: data.password } });
+    if (!user) throw new Error();
+    const newMatch = await this.matchesModel.create({ ...match, inProgress: true });
+    return newMatch;
   }
 }
